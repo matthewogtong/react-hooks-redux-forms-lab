c01aa4b0118b0c811d1e2f103342105f3f1bc17f
matthewogtong <ogtongm@gmail.com>
2021-02-28 21:26:22 -0500

    Done.

--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux"
+import BandInput from "./BandInput"
+import { bandAdded } from "./bandsSlice"
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+
+  const dispatch = useDispatch()
+  const bands = useSelector(state => state.bands.entities)
+
+  const onBandSubmit = (name) => {
+    dispatch(bandAdded(name))
+  }
+
+  const displayBands = bands.map(band => {
+    return (
+      <li key={band}>{band}</li>
+    )
+  })
+
+  return (
+    <div>
+      <BandInput onBandSubmit={onBandSubmit} />
+      <ul>
+        {displayBands}
+      </ul>
+    </div>
+  )
 }
 
 export default BandsContainer;
